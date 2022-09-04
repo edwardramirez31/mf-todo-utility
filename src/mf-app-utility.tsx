a4d3dd04516bef4b3ce586fa26cec4911b959be2
Edward Ramirez <Edward.Ramirez@coxautoinc.com>
2022-09-04 13:26:55 -0500

    feat: publish package to npm using the pipeline

--- src/mf-app-utility.tsx
+++ src/mf-app-utility.tsx
@@ -1,12 +1,11 @@
 // Anything exported from this file is importable by other in-browser modules.
-
-import { ReplaySubject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 import type DjangoTask from './types';
 
-export const UTILITY_CONST = 'YOUR_CONSTANT';
+export const UTILITY_CONST = 'CONSTANT';
 
-export const taskSubscription = new ReplaySubject<DjangoTask>();
+export const taskSubscription = new Subject<DjangoTask>();
 
 export function utilityFunc(item: DjangoTask): void {
   // eslint-disable-next-line no-console
